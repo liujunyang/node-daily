701c3bbc89dddf8ac0c15bd59251d9d0b342eb70
liujunyang <liujunyangscript@gmail.com>
2018-04-11 18:21:24 +0800

    add querystring

---
+++ 4116/am/server_cookie3.js
@@ -0,0 +1,24 @@
+let http = require('http')
+let querystring = require('querystring')
+let server = http.createServer((req, res) => {
+	console.log(req.url)
+
+	if (req.url === '/favicon.ico') {
+		res.end('')
+		return;
+	}
+
+	console.log(req.headers.cookie)
+
+	let cookie = {}
+
+	if (req.headers.cookie) {
+		cookie = querystring.parse(req.headers.cookie.replace(/; /g, '&'))
+	}
+
+	console.log(99, cookie)
+
+	res.end('abab')
+})
+
+server.listen(8080)
