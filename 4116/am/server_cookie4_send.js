d5d522c25f8d2d2a892594a2274faecfdce983be
liujunyang <liujunyangscript@gmail.com>
2018-04-14 15:19:01 +0800

    writeHeader

---
+++ 4116/am/server_cookie4_send.js
@@ -0,0 +1,12 @@
+let http = require('http')
+let querystring = require('querystring')
+let server = http.createServer((req, res) => {
+	if (req.url === '/favicon.ico') {return res.end();}
+
+	res.writeHeader(200, {'set-cookie': 'a=12'})
+	res.write('aaa')
+	res.end()
+
+})
+
+server.listen(8080)
