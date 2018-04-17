207a6171fa828e1fad3c2ca5e05b0062d4f1d49a
liujunyang <liujunyangscript@gmail.com>
2018-04-17 21:30:03 +0800

    setHeader

--- 4116/am/server_cookie4_send2.js
+++ 4116/am/server_cookie4_send2.js
@@ -3,7 +3,9 @@ let querystring = require('querystring')
 let server = http.createServer((req, res) => {
 	if (req.url === '/favicon.ico') {return res.end();}
 
-	res.writeHeader(200, {'set-cookie': ['a=12', 'b=5']})
+	// res.writeHeader(200, {'set-cookie': ['a=12', 'b=5']})
+
+	res.setHeader('set-cookie', ['a=12', 'b=5'])
 	res.write('aaa')
 	res.end()
 
