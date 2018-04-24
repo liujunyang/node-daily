9131d63b0e96ed79d51b8d8d956253c6b6521870
liujunyang <liujunyangscript@gmail.com>
2018-04-24 18:37:10 +0800

    add data2

---
+++ 4116/am/server_data2.js
@@ -0,0 +1,33 @@
+   
+
+var http=require('http');
+var querystring=require('querystring');
+var urlLib=require('url');
+
+http.createServer(function (req, res){
+	var str='';		//风险
+	
+	req.on('data', function (data){
+		str+=data;
+	});
+	req.on('end', function (){
+		//解析
+		//GET
+		var json=urlLib.parse(req.url, true);
+		var url=json.pathname;
+		var get=json.query;
+		
+		//POST
+		var post=querystring.parse(str);
+		
+		console.log(url, get, post);
+	});
+}).listen(80);
+
+
+
+
+
+
+
+
