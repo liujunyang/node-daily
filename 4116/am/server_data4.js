c2734e0300930f945f1c04bf8b62f64affb1be6d
liujunyang <liujunyangscript@gmail.com>
2018-05-03 18:31:06 +0800

    add data4

---
+++ 4116/am/server_data4.js
@@ -0,0 +1,18 @@
+   
+
+var http=require('http');
+var dataParse=require('dataParse');
+
+http.createServer(function (req, res){
+	dataParse.parse(req, function (url, get, post, cookie){
+		console.log(url, get, post, cookie);
+	});
+}).listen(80);
+
+
+
+
+
+
+
+
