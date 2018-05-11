df2b29a4272af70392bbc65ff61e36b837e5227a
liujunyang <liujunyangscript@gmail.com>
2018-05-11 18:37:29 +0800

    add get-data

---
+++ 4116/am/server_get_data.js
@@ -0,0 +1,22 @@
+   
+
+var http=require('http');
+var fs=require('fs');
+
+http.createServer(function (req, res){
+	console.log(req.url);
+	
+	/*
+	res.write('asdfasd');
+	res.end();
+	*/
+	
+	res.end('aaaa');
+}).listen(80);
+
+
+
+
+
+
+
