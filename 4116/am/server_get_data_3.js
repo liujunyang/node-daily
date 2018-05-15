508464641f9e94a6683f101bc2731529fad8bf24
liujunyang <liujunyangscript@gmail.com>
2018-05-15 18:21:15 +0800

    data3

---
+++ 4116/am/server_get_data_3.js
@@ -0,0 +1,26 @@
+   
+
+var http=require('http');
+var fs=require('fs');
+var urlLib=require('url');
+
+http.createServer(function (req, res){
+	console.log(req.url);
+	
+	var obj=urlLib.parse(req.url, true);
+	
+	var url=obj.pathname;
+	var get=obj.query;
+	
+	console.log('解析后的=========');
+	console.log(url, get);
+	
+	res.end('aaaa');
+}).listen(80);
+
+
+
+
+
+
+
