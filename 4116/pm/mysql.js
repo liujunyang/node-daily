b89ed9cd94ef8d7d2ccfb3982772525e3c42c0f9
liujunyang <liujunyangscript@gmail.com>
2018-05-25 19:14:32 +0800

    add mysql

---
+++ 4116/pm/mysql.js
@@ -0,0 +1,15 @@
+var mysql=require('mysql');
+
+//1.连接
+//2.请求
+
+//1.连接-createConnection(地址, 用户名, 密码, 库)
+var db=mysql.createConnection({host: 'localhost', user: 'root', password: '', database: 'test'});
+
+//2.请求-query(sql语句, 回调)
+db.query('SELECT * FROM user_table', function (err, data){
+	if(err)
+		console.log(err);
+	else
+		console.log(data);
+});
