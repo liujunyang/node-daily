73c7d1da7c28eabc879986e0f0db51f3b3637732
liujunyang <liujunyangscript@gmail.com>
2018-05-25 19:52:09 +0800

    add 库

--- 4116/pm/mysql.js
+++ 4116/pm/mysql.js
@@ -4,7 +4,7 @@ var mysql=require('mysql');
 //2.请求
 
 //1.连接-createConnection(地址, 用户名, 密码, 库)
-var db=mysql.createConnection({host: 'localhost', user: 'root', password: '', database: 'test'});
+var db=mysql.createConnection({host: 'localhost', user: 'root', password: '123456', database: 'test'});
 
 //2.请求-query(sql语句, 回调)
 db.query('SELECT * FROM user_table', function (err, data){
