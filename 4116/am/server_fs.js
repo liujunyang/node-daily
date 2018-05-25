f5306da8671c769c4c9d308fe7c8d6f76c830b78
liujunyang <liujunyangscript@gmail.com>
2018-05-25 18:39:49 +0800

    add www

--- 4116/am/server_fs.js
+++ 4116/am/server_fs.js
@@ -1,21 +1,21 @@
-   
-
 var http=require('http');
 var fs=require('fs');
 
 var server=http.createServer(function (request, response){
+	console.log(process.cwd())
 	//url	->	'/1.html'		'www/1.html'
-	fs.readFile('www'+request.url, function (err, data){
-		if(err)
+	fs.readFile('./4116/am/www'+request.url, function (err, data){
+		if(err){
 			response.write('错误');
-		else
+		} else {
 			response.write(data);
-		
+		}
+
 		response.end();
 	});
 });
 
-server.listen(80);
+server.listen(8080);
 
 
 
