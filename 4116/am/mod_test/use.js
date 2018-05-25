2715f95f1616fad24ba7e9e98231a6b82fa30788
liujunyang <liujunyangscript@gmail.com>
2018-05-25 19:09:38 +0800

    核心模块优先加载

---
+++ 4116/am/mod_test/use.js
@@ -0,0 +1,4 @@
+//var a=require('D:\\workspace_nodejs\\2014-11-16\\mod_test\\mod.js');
+var a=require('./mod.js');
+
+console.log(a.a, a.b, a.c);
---
+++ 4116/am/mod_test2/user.js
@@ -0,0 +1,3 @@
+var obj=require('a');
+
+console.log(obj.a);
